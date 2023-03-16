af348363f0061c239137407af3bc72a1cd8d3d77
Enoch Asante <64912831+Carter23232@users.noreply.github.com>
2023-03-16 17:45:53 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    const char *msg = "and that piece of art is useful
+        \" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, msg, strlen(msg));
     return 1;
 }
