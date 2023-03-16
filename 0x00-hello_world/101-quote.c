bf0edd8f224eebffb89d5f0da738121553e41100
Enoch Asante <64912831+Carter23232@users.noreply.github.com>
2023-03-16 20:18:55 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, msg, strlen(msg));
+    const char *msg;
+    msg="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, msg, strlen(msg));
     return (1);
 }
