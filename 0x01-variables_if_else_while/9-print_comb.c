2acdb2382ab6b45e350d3a048dd4c7e1df9dae72
Enock Asant <asanteenoch10@gmail.com>
2023-03-17 16:59:19 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+  * main - Entry point
+  * Return: Always 1 (Success)
+  */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+	putchar(a);
+	putchar(44);
+	putchar(32);
+}
+putchar(10);
+return (0);
+}
