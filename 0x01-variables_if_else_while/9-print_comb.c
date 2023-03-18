ad61edf578eb1d2e921ca9a6286a3e20b21d2960
Enock Asant <asanteenoch10@gmail.com>
2023-03-17 17:05:13 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,14 @@
 int main(void)
 {
 int a;
-for (a = 48; a <= 57; ++a)
+for (a = 48; a <= 57; a++)
 {
 	putchar(a);
-	putchar(44);
-	putchar(32);
+	if (a<57)
+	{
+		putchar(44);
+		putchar(32);
+	}	
 }
 putchar(10);
 return (0);
