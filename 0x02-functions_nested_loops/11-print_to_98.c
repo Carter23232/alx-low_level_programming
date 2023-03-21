50c46dd36fde012f61c068262c50d65a724d51a5
Enock Asant <asanteenoch10@gmail.com>
2023-03-21 15:00:41 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_to_98 - prints all natural numbers from n to 98.
+* @n: - starting point
+*
+*/
+void print_to_98(int n)
+{
+int num, fdig, ldig;
+	for (num = n; num <= 98; num++)
+	{
+		if (num < 9)
+		{
+			_putchar(num + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			ldig = num % 10;
+			fdig = (num - ldig) / 10;
+			_putchar(fdig + '0');
+			_putchar(ldig + '0');
+		}
+	}
+}
