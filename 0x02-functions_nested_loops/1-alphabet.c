8a24b8cb70bb388d97f16c6c972a1a8dcce9007e
Enock Asant <asanteenoch10@gmail.com>
2023-03-21 09:00:09 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-/* more headers goes there */
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-/* betty style doc for function main goes there */
-int main(void)
+void print_alphabet(void)
 {
 unsigned long i;
 for (i = 97; i < 122; i++)
@@ -15,5 +7,4 @@ for (i = 97; i < 122; i++)
 	_putchar((char)i);
 }
 _putchar('\n');
-return (0);
 }
