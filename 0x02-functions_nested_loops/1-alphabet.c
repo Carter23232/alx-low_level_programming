93556e8422cc64b0c7de3066fd6b84d418734848
Enock Asant <asanteenoch10@gmail.com>
2023-03-21 06:00:59 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,8 @@
   * main - Entry point
    * Return: Always 1 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
-int a;
-for (a = 97; a <= 122; a++)
-{
-	putchar(a);
-}
-putchar(10);
+	print_alphabet();
+	return (0);
 }
