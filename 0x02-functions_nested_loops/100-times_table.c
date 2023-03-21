9df188c4c50abb22388e6c1f78a0c21559020202
Enock Asant <asanteenoch10@gmail.com>
2023-03-21 16:04:56 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,17 +7,44 @@
   */
 void print_times_table(int n)
 {
-int i, j;
-for  (i = 0; i <= n; i++)
-{
-	for (j = 0; j <= n; j++)
+int x, y, z;
+
+	if (n >= 0 && n <= 14)
 	{
-		printf("%d", i * j);
-		if (j != n)
+		for (x = 0; x <= n; x++)
 		{
-			printf(",  ");
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
 		}
 	}
-	printf("\n");
-}
 }
