e6a65780602d7bfc2099242a3d880ff6ae8a8655
Enock Asant <asanteenoch10@gmail.com>
2023-03-27 16:08:50 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse.
+ * @s: string to print
+ *
+ *
+ */
+void print_rev(char *s)
+{
+unsigned long i;
+for (i = strlen(s); i >= 0; i--)
+{
+	_putchar(s[i]);
+}
+_putchar('\n');
+}
