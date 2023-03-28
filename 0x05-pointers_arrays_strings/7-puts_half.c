3cab03cc28d2c930229700f7a22d37d5a1eb3484
Enock Asant <asanteenoch10@gmail.com>
2023-03-27 17:43:37 -0700

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * puts_half - Prints half of a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void puts_half(char *str)
+{
+int j = 0, i;
+while (str[j] != '\0')
+{
+	j++;
+}
+for (i = j / 2; i < j; i++)
+{
+	_putchar(str[i]);
+}
+_putchar('\n');
+}
