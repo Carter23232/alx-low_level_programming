d79858aed872ca52436ff978eb0d30046c5c58db
Enock Asant <asanteenoch10@gmail.com>
2023-03-28 03:35:35 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
  */
 void print_rev(char *s)
 {
-int counter = 0;
+	int counter = 0, i;
 	while (s[counter] != '\0')
 	{
 		counter++;
 	}
-	for (counter -= 1; counter >= 0; counter--)
+	for (i = counter - 1; i >= 0; i--)
 	{
-		_putchar(s[counter]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
