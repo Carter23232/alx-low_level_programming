bb7ad105d938971ce1352e4bd568dd3cc828a119
Enock Asant <asanteenoch10@gmail.com>
2023-03-28 15:56:05 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,9 @@
   */
 void rev_string(char *s)
 {
-int len = strlen(s);
+int len = strlen(s), i;
 char temp;
-for (int i = 0; i < len / 2; i++)
+for (i = 0; i < len / 2; i++)
 {
 	temp = s[i];
 	s[i] = s[len - i - 1];
