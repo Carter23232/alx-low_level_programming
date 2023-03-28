4591ad84961d8bc1efa71882c543a24b4e8d82b5
Enock Asant <asanteenoch10@gmail.com>
2023-03-28 04:23:23 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,21 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
   * rev_string - Reverses a string
   * @s: The string to be modified
   *
   * Return: void
   */
+
 void rev_string(char *s)
 {
 int i, j, counter = 0;
-
+char *rev;
 	while (s[counter] != '\0')
 	{
 		counter++;
 	}
-char rev[counter];
+	rev = (char *)malloc(counter * sizeof(char));
 	for (i = 0, j = counter - 1; i < counter; i++, j--)
 	{
 		rev[j] = s[i];
