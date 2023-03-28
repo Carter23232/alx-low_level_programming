77938d21b6712cff2884881003cd2ae07c476e32
Enock Asant <asanteenoch10@gmail.com>
2023-03-27 18:37:47 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,24 +4,25 @@
 
 #define MAX_LEN 100
 
-char *generate_random_string(int sum) {
-    char *str = malloc(sizeof(char) * (MAX_LEN + 1));
-    int len = 0;
-    srand(time(NULL));
-    while (sum > 0 && len < MAX_LEN) {
-  int n = rand() % 79 + 48;
-        if (n > sum) continue; 
-        str[len++] = n;
-        sum -= n;
-    }
-    str[len] = '\0';
-    return str;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_LEN 100
 
 int main() {
-	int sum = 234;
-    char *str = generate_random_string(sum);
-    printf("%s\n", str);
-    free(str);
+    int target_sum = 234;  // example sum
+    char *result_str = malloc(sizeof(char) * (MAX_LEN + 1));
+    int len = 0, num = 0;
+    srand(time(NULL));
+    while (target_sum > 0 && len < MAX_LEN) {
+        num = rand() % 79 + 48;  // generate random number in ASCII range [48, 126], excluding control characters
+        if (num > target_sum) continue;  // skip if number is greater than remaining sum
+        result_str[len++] = num;
+        target_sum -= num;
+    }
+    result_str[len] = '\0';
+    printf("%s\n", result_str);
+    free(result_str);
     return 0;
 }
