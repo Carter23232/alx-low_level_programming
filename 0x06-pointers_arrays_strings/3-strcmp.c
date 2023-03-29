b265fd98d210d08d1c172ab14925733dfbeb8a64
Enock Asant <asanteenoch10@gmail.com>
2023-03-29 13:42:54 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strcmp - compares s1 and s2
+ * @s1: lhs
+ * @s2 : rhs
+ *
+ * Return: returns copied string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int len_s1 = (int)strlen(s1), len_s2 = (int)strlen(s2);
+	int i, s1sum = 0, s2sum = 0;
+
+	for (i = 0; i < len_s1; i++)
+	{
+		s1sum += (int)s1[i];
+	}
+	for (i = 0; i < len_s2; i++)
+	{
+		s2sum += (int)s2[i];
+	}
+	if (s1sum > s2sum)
+	{
+		return (1);
+	}
+	else if (s1sum == s2sum)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
