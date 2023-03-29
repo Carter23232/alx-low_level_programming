d0e16e082be5639c61904f323c2406ff2ccc6140
Enock Asant <asanteenoch10@gmail.com>
2023-03-29 12:24:01 -0700

    update

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - copies a char from src to a dest
+ * @dest: destination
+ * @src : source
+ * @n: length of string to copy from src
+ * Return: returns copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int s_size = (int) strlen(src), i, j;
+
+if (n >= s_size)
+{
+	for (i = 0, j = 0; i < s_size; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+}
+else
+{
+	for (i = 0, j = 0; i < n; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+}
+return (dest);
+}
