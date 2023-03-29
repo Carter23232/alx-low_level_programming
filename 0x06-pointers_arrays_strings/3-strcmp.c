62b245c1f4303794386c2521549c96d7cf9da030
Enock Asant <asanteenoch10@gmail.com>
2023-03-29 14:27:58 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,27 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1 = (int)strlen(s1), len_s2 = (int)strlen(s2);
-	int i, s1sum = 0, s2sum = 0;
+	int len_s1 = (int)strlen(s1), len_s2 = (int)strlen(s2), i;
+	int len = (len_s1 > len_s2) ? len_s1 : len_s2;
 
-	for (i = 0; i < len_s1; i++)
+	for (i = 0; i < len; i++)
 	{
-		s1sum += (int)s1[i];
-	}
-	for (i = 0; i < len_s2; i++)
-	{
-		s2sum += (int)s2[i];
-	}
-	if (s1sum > s2sum)
-	{
-		return (s1sum - s2sum);
-	}
-	else if (s1sum == s2sum)
-	{
-		return (0);
-	}
-	else
-	{
-		return (s1sum - s2sum);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 }
