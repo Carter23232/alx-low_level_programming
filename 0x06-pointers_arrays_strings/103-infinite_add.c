74ba136d44d7f248fbd45181a238c35b07b0338c
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 16:26:19 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,58 +10,33 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
-	char tmp[10000];
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int carry = 0;
+	int i = len1 - 1;
+	int j = len2 - 1;
+	int k = 0;
 
-	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
-	while (n1[l1] != '\0')
-		l1++;
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 + 2 > size_r || l2 + 2 > size_r)
-		return (0);
-	l1--;
-	l2--;
-	while (i <= l1 || i <= l2)
+	while (i >= 0 || j >= 0 || carry != 0)
 	{
-		num1 = num2 = 0;
-		if (i <= l1)
-			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0)
-			num2 = n2[l2 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-		{
-			carry = 1;
-			sum -= 10;
-		}
-		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		rl++;
+		int sum = carry;
+
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+		if (k >= size_r)
+			return (0);
+		r[k++] = sum % 10 + '0';
+		carry = sum / 10;
 	}
-	if (carry > 0)
+	for (int i = 0, j = k - 1; i < j; i++, j--)
 	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
+		char temp = r[i];
+
+		r[i] = r[j];
+		r[j] = temp;
 	}
-	i = tmpl = 0;
-	while (i <= rl)
-	{
-		tmp[i] = r[rl - i];
-		tmpl++;
-		i++;
-	}
-	i = 0;
-	while (i < tmpl)
-	{
-		if (r[i] == '\0')
-		{
-			break;
-		}
-		r[i] = tmp[i];
-		i++;
-	}
+	r[k] = '\0';
 	return (r);
 }
