630b30f1dcb92b0383d0df7b68636c3a0e9d4345
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 16:13:35 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,40 +7,44 @@
  *
  * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int carry = 0, sum, len1, len2, i;
-    char *ptr1 = n1, *ptr2 = n2, *res_ptr = r + size_r - 1;
-    
-    // Calculate lengths of input strings
-    for (len1 = 0; n1[len1]; len1++);
-    for (len2 = 0; n2[len2]; len2++);
-    
-    // Iterate over the strings in reverse
-    while (len1 || len2 || carry) {
-        sum = carry;
-        if (len1) {
-            sum += ptr1[--len1] - '0';
-            ptr1[len1] = sum % 10 + '0';
-        }
-        if (len2) {
-            sum += ptr2[--len2] - '0';
-            ptr2[len2] = sum % 10 + '0';
-        }
-        if (res_ptr == r) {
-            return (0);
-        }
-        *--res_ptr = sum % 10 + '0';
-        carry = sum / 10;
-    }
-    
-    for (i = 0; r[i] == '0' && i < size_r - 1; i++);
-    for (int j = 0; i < size_r; i++, j++) {
-        if (r[i] == '\0') {
-            r[j] = '\0';
-            break;
-        }
-        r[j] = r[i];
-    }
-    return (r);
+% 10) + '0';
+size_r--;
+i--;
+j--;
 }
+
+if (carry)
+{
+	if (size_r == 0)
+		return (0);
+	r[size_r - 1] = carry + '0';
+	size_r--;
+}
+
+if (size_r == 0 || (r[0] == '0' && r[1] != '\0'))
+	      char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    int carry = 0, i, j;
+    int len1 = 0, len2 = 0, sum = 0;
+
+    while (n1[len1])
+        len1++;
+    while (n2[len2])
+        len2++;
+    if (len1 > size_r || len2 > size_r)
+        return (0);
+
+    r[size_r - 1] = '\0';
+    i = len1 - 1;
+    j = len2 - 1;
+    size_r--;
+
+    while (i >= 0 || j >= 0)
+    {
+        sum = carry + (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j] - '0' : 0);
+        carry = sum / 10;
+        r[size_r - 1] = (sum   return (0);
+
+    return (r + size_r);
+}
+
