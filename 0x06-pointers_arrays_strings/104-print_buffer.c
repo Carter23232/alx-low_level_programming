7c55d64d2f6063bb20ceb530dd0ab337e285a18b
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 15:59:18 -0700

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,33 +3,32 @@
 
 void print_buffer(char *b, int size)
 {
-    if (size <= 0) {
-        printf("\n");
-        return;
-    }
-
     int i, j;
+
     for (i = 0; i < size; i += 10) {
         printf("%08x: ", i);
         for (j = 0; j < 10; j++) {
             if (i + j < size) {
-                printf("%02x", (unsigned char)b[i+j]);
+                printf("%02x", *(b + i + j));
             } else {
                 printf("  ");
             }
-            if (j % 2 == 1) {
+            if (j % 2 != 0) {
                 printf(" ");
             }
         }
         for (j = 0; j < 10; j++) {
             if (i + j < size) {
-                if (isprint(b[i+j])) {
-                    printf("%c", b[i+j]);
+                if (isprint(*(b + i + j))) {
+                    printf("%c", *(b + i + j));
                 } else {
                     printf(".");
                 }
             }
         }
+        printf("\n");
+    }
+    if (size <= 0) {
         printf("\n");
     }
 }
