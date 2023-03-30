e4816bae29a5dee1fa324d1e00c5e4a2b612c58b
Enock Asant <asanteenoch10@gmail.com>
2023-03-29 17:14:59 -0700

    update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+  * leet - encode a string to 1337.
+  * @a: character to cahnge
+  *
+  * Return: returns modified string
+*/
+char *leet(char *a)
+{
+	char *let = "aeotl";
+	int num[] = {4, 3, 0, 7, 1};
+	int i, j;
+
+	for (i = 0; i < (int)strlen(let); i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (let[i] == a[j] || toupper(let[i]) == a[j])
+			{
+				a[j] = num[i];
+				j++;
+			}
+		}
+	}
+	return (a);
+}
