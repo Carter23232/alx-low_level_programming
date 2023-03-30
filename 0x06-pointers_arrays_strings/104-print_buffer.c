3c770d0e19f6041e5ead329b1258a372d725e621
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 16:07:50 -0700

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,34 +1,54 @@
 #include <stdio.h>
 #include <ctype.h>
-
+/**
+ * print_buffer - ...
+ * @b: The ..
+ * @size: The second number
+ *
+ *
+ *
+ *
+ */
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < size; i += 10) {
-        printf("%08x: ", i);
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                printf("%02x", *(b + i + j));
-            } else {
-                printf("  ");
-            }
-            if (j % 2 != 0) {
-                printf(" ");
-            }
-        }
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                if (isprint(*(b + i + j))) {
-                    printf("%c", *(b + i + j));
-                } else {
-                    printf(".");
-                }
-            }
-        }
-        printf("\n");
-    }
-    if (size <= 0) {
-        printf("\n");
-    }
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				printf("%02x", *(b + i + j));
+			}
+			else
+			{
+				printf("  ");
+			}
+			if (j % 2 != 0)
+			{
+				printf(" ");
+			}
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (isprint(*(b + i + j)))
+				{
+					printf("%c", *(b + i + j));
+				}
+				else
+				{
+					printf(".");
+				}
+			}
+		}
+		printf("\n");
+	}
+	if (size <= 0)
+	{
+		printf("\n");
+	}
 }
