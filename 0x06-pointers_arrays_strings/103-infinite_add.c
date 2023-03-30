a28392afcd6a8d7dc58b819a54f934e05466bf87
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 16:21:28 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * infinite_add - Add up two numbers stored in given char arrays
  * @n1: The first number
@@ -7,22 +8,7 @@
  *
  * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
-% 10) + '0';
-size_r--;
-i--;
-j--;
-}
-
-if (carry)
-{
-	if (size_r == 0)
-		return (0);
-	r[size_r - 1] = carry + '0';
-	size_r--;
-}
-
-if (size_r == 0 || (r[0] == '0' && r[1] != '\0'))
-	      char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int carry = 0, i, j;
     int len1 = 0, len2 = 0, sum = 0;
@@ -43,8 +29,22 @@ if (size_r == 0 || (r[0] == '0' && r[1] != '\0'))
     {
         sum = carry + (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j] - '0' : 0);
         carry = sum / 10;
-        r[size_r - 1] = (sum   return (0);
+        r[size_r - 1] = (sum % 10) + '0';
+        size_r--;
+        i--;
+        j--;
+    }
+
+    if (carry)
+    {
+        if (size_r == 0)
+            return (0);
+        r[size_r - 1] = carry + '0';
+        size_r--;
+    }
+
+    if (size_r == 0 || (r[0] == '0' && r[1] != '\0'))
+        return (0);
 
     return (r + size_r);
 }
-
