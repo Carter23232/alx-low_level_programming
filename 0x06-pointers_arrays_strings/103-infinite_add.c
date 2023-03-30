2134377d12497e43f205b9f3074df98356c7e40e
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 16:31:39 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,50 +11,58 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int carry = 0, index = 0;
-    char *s1 = n1 + strlen(n1) - 1, *s2 = n2 + strlen(n2) - 1;
+    char *s1 = n1 + _strlen(n1) - 1, *s2 = n2 + _strlen(n2) - 1;
 
-    while (s2 >= n2 && s1 >= n1) {
+    while (s2 >= n2 && s1 >= n1)
+    {
         r[index] = (*s2 - '0') + (*s1 - '0') + carry;
         carry = r[index] / 10;
         r[index] %= 10;
         s1--;
         s2--;
         index++;
-        if (index == size_r) return NULL;
+        if (index == size_r)
+            return (NULL);
     }
 
-    while (s1 >= n1) {
+    while (s1 >= n1)
+    {
         r[index] = (*s1 - '0') + carry;
         carry = r[index] / 10;
         r[index] %= 10;
         s1--;
         index++;
-        if (index == size_r) return NULL;
+        if (index == size_r)
+            return (NULL);
     }
 
-    while (s2 >= n2) {
+    while (s2 >= n2)
+    {
         r[index] = (*s2 - '0') + carry;
         carry = r[index] / 10;
         r[index] %= 10;
         s2--;
         index++;
-        if (index == size_r) return NULL;
+        if (index == size_r)
+            return (NULL);
     }
 
-    if (carry == 1) {
-        if (index == size_r) return NULL;
+    if (carry == 1)
+    {
+        if (index == size_r)
+            return (NULL);
         r[index] = 1;
         index++;
     }
 
     r[index] = '\0';
 
-    for (int i = 0; i < index / 2; i++) {
+    for (int i = 0; i < index / 2; i++)
+    {
         char tmp = r[i];
         r[i] = r[index - i - 1];
         r[index - i - 1] = tmp;
     }
 
-    return r;
+    return (r);
 }
-
