2ed3c8c39db2bdc690b44da0a30d2978fef4b287
Enock Asant <asanteenoch10@gmail.com>
2023-03-30 15:35:52 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,37 +11,33 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
-    int carry = 0;
-    int i = len1 - 1;
-    int j = len2 - 1;
-    int k = 0;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int carry = 0;
+	int i = len1 - 1;
+	int j = len2 - 1;
+	int k = 0;
 
-    // Iterate through the two input strings, adding their corresponding digits
-    // and any carry from the previous digit, and storing the result in the
-    // output buffer.
-    while (i >= 0 || j >= 0 || carry != 0)
-    {
-        int sum = carry;
-        if (i >= 0)
-            sum += n1[i--] - '0';
-        if (j >= 0)
-            sum += n2[j--] - '0';
-        if (k >= size_r)
-            return 0;
-        r[k++] = sum % 10 + '0';
-        carry = sum / 10;
-    }
+	while (i >= 0 || j >= 0 || carry != 0)
+	{
+		int sum = carry;
 
-    
-    for (int i = 0, j = k - 1; i < j; i++, j--)
-    {
-        char temp = r[i];
-        r[i] = r[j];
-        r[j] = temp;
-    }
-    r[k] = '\0';
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+		if (k >= size_r)
+			return (0);
+		r[k++] = sum % 10 + '0';
+		carry = sum / 10;
+	}
+	for (int i = 0, j = k - 1; i < j; i++, j--)
+	{
+		char temp = r[i];
 
-    return r;
+		r[i] = r[j];
+		r[j] = temp;
+	}
+	r[k] = '\0';
+	return (r);
 }
