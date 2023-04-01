3cde466fec705ab7e3a34e2ed9a049b143cfe674
Enock Asant <asanteenoch10@gmail.com>
2023-04-01 10:40:30 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _pow - Power of a number
- * @num: base
- * @power: power
- * Return: result
- */
-
-double _pow(int num, int power)
-{
-	int i, result = 1;
-
-	for (i = 0; i < power; i++)
-	{
-		result *= num;
-	}
-	return (result);
-}
-
-/**
   * print_number - Prints any integer with putchar
   * @n: Number to prints
   * Return: Nothing
@@ -26,23 +8,15 @@ double _pow(int num, int power)
 
 void print_number(int n)
 {
-	int counter = 0, number_dup = n, first_digit;
-
-	while ((number_dup /= 10))
+	if (n < 0)
 	{
-		counter++;
+		_putchar('-');
+		n = -n;
 	}
-	while (counter >= 0)
+	if (n >= 10)
 	{
-		if (n < 0)
-		{
-			n *= -1;
-			_putchar('-');
-		}
-		first_digit = n / (int)_pow(10, counter);
-		first_digit %= 10;
-		counter--;
-		_putchar(first_digit + '0');
+		print_number(n / 10);
 	}
+	_putchar((n % 10) + '0');
 }
 
