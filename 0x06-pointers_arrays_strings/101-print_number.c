891954c2e2d24c12b8671150ad8c106c4062a7fb
Enock Asant <asanteenoch10@gmail.com>
2023-04-01 07:55:57 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <math.h>
 /**
   * print_number - Prints any integer with putchar
   * @n: Number to prints
@@ -8,18 +8,22 @@
   */
 void print_number(int n)
 {
-	unsigned int x;
+	int counter = 0, number_dup = n, first_digit;
 
-	if (n < 0)
+	while ((number_dup /= 10))
 	{
-		_putchar('-');
-		n *= -1;
+		counter++;
 	}
-
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	while (counter >= 0)
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			putchar('-');
+		}
+		first_digit = n / (int)pow(10, counter);
+		first_digit %= 10;
+		counter--;
+		putchar(first_digit + '0');
+	}
 }
