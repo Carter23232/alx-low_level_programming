52dacc80dfad726fd4626649157fccf0ab31329c
Enock Asant <asanteenoch10@gmail.com>
2023-04-02 05:20:41 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
-  * _pow - calculates the pow of a number
-  * @b: base
-  * @p: power
-  * Return: result
-  */
-
-int _pow(int b, int p)
-{
-	int i, result = 1;
-
-	for (i = 0; i < p; i++)
-	{
-		result *= b;
-	}
-	return (result);
-}
-
-/**
   * print_number - Prints any integer with putchar
   * @n: Number to prints
   * Return: Nothing
@@ -26,28 +8,16 @@ int _pow(int b, int p)
 
 void print_number(int n)
 {
-	int nod = 0;
-	unsigned int first_digit = 0;
-	unsigned int abs_n = n < 0 ? -n : n;
+	unsigned int num = n;
 
-	if (n == 0)
-	{
-		putchar('0');
-		return;
-	}
-	while (abs_n >= (unsigned int)_pow(10, nod + 1))
-	{
-		nod++;
-	}
-	while (nod >= 0)
-	{
-		first_digit = abs_n / _pow(10, nod);
-		first_digit %= 10;
-		nod--;
-		_putchar(first_digit + '0');
-	}
 	if (n < 0)
 	{
 		_putchar('-');
+		num = -num;
 	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
