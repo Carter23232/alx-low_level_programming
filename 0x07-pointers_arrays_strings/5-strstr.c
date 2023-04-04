a865c6300ae406957eb922a0eb35f16f33f7cd57
Enock Asant <asanteenoch10@gmail.com>
2023-04-04 01:00:27 -0700

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
-  * _strpbrk - search a string for any of a set of bytes
-  * @s: source string
-  * @accept: accepted characters
+  * _strstr - function that locates a substring
+  * @haystack: source string
+  * @needle: accepted characters
   *
   * Return: the string since the first found accepted character
   */
+
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		if (strncmp(haystack,needle,strlen(needle)) == 0)
+		if (strncmp(haystack, needle, strlen(needle)) == 0)
 		{
 			return (haystack);
 		}
