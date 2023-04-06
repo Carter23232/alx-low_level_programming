dae58f25499f389014ee8a577c19088470e5f5fb
Enock Asant <asanteenoch10@gmail.com>
2023-04-05 19:19:11 -0700

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,13 +24,17 @@ int cmp(char *s1, char *s2)
 	{
 		return (1);
 	}
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		{
+			return (cmp(s1, s2 + 1));
+		}
+		return (cmp(s1 + 1, s2) || cmp(s1, s2 + 1));
+	}
 	if (*s1 == *s2)
 	{
 		return (cmp(s1 + 1, s2 + 1));
-	}
-	if (*s2 == '*')
-	{
-		return (cmp(s1, s2 + 1) || cmp(s1 + 1, s2));
 	}
 	return (0);
 }
