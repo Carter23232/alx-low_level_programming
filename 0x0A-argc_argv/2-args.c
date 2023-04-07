802c0206bb5c380d4c892e887906d2c5f0198e44
Enock Asant <asanteenoch10@gmail.com>
2023-04-07 11:07:52 -0700

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,7 +8,11 @@
  */
 int main(int argc, char **argv)
 {
-	printf("%i\n", argc);
+        int i;
 
-	return (0);
+        for (i = 0; i < argc; i++)
+        {
+                printf("%s\n", argv[i]);
+        }
+        return (0);
 }
