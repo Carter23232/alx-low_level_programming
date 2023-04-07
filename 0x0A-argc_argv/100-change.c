05e42a421e61c2b1105c70e34ce55d6be4a1113c
Enock Asant <asanteenoch10@gmail.com>
2023-04-07 15:47:31 -0700

    update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - check the code
+ *@argc: length of string
+ *@argv: array of string
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int c_val[] = {25, 10, 5, 2, 1};
+	int i, num = atoi(argv[1]), counter = 0;
+
+	if (argc == 2)
+	{
+		if (*argv[1] < 0)
+		{
+			printf("%d\n", 0);
+		}
+		else
+		{
+			for (i = 0; i < 5;)
+			{
+				if (num > c_val[i])
+				{
+					num -= c_val[i];
+					counter++;
+				}
+				else
+				{
+					i++;
+				}
+			}
+			printf("%d\n", counter);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
