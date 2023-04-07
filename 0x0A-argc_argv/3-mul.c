f9a6610cf3498bd128ed52f06781f5f6475e1cec
Enock Asant <asanteenoch10@gmail.com>
2023-04-07 11:24:48 -0700

    update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *@argc: length of string
+ *@argv: array of string
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, prod = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		prod *= i;
+	}
+	printf("%d\n", prod);
+	return (0);
+}
