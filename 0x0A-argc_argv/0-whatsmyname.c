6cd40db0091f2e922ee5a5f17a2547c1cd897238
Enock Asant <asanteenoch10@gmail.com>
2023-04-07 10:53:29 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *@argc: length of string
+ *@argv: array of string
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
