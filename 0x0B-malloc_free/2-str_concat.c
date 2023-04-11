21637b1ed6a3604a94577824ec4e275781c7abe3
Enock Asant <asanteenoch10@gmail.com>
2023-04-11 16:48:17 -0700

    update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ *str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *Return: ptr to concantated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *constr;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return ("");
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+		constr = (char *)(malloc(strlen(s1) * sizeof(char) + 1));
+		if (constr != NULL)
+		{
+			strcpy(constr, s1);
+			return (constr);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		constr = (char *)(malloc(strlen(s2) * sizeof(char) + 1));
+		if (constr != NULL)
+		{
+			strcpy(constr, s2);
+			return (constr);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	constr = (char *)(malloc((strlen(s1) + strlen(s2)) * sizeof(char) + 1));
+	if (constr != NULL)
+	{
+		strcpy(constr, s1);
+		strcat(constr, s2);
+		return (constr);
+	}
+	return (NULL);
+}
