40bcd508062ca3f92a0c99b8529f6b22838656d0
Enock Asant <asanteenoch10@gmail.com>
2023-04-11 17:57:58 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,35 +8,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int s1_length = 0, s2_length = 0;
-    char *result, *p;
+	int s1_length = 0, s2_length = 0;
+	char *result, *p;
 
-    if (s1 != NULL) {
-        s1_length = strlen(s1);
-    }
-
-    if (s2 != NULL) {
-        s2_length = strlen(s2);
-    }
-
-    result = (char *)malloc(s1_length + s2_length + 1);
-
-    if (result == NULL) {
-        return NULL;
-    }
-
-    p = result;
-
-    while (s1 != NULL && *s1 != '\0') {
-        *p++ = *s1++;
-    }
-
-    while (s2 != NULL && *s2 != '\0') {
-        *p++ = *s2++;
-    }
-
-    *p = '\0';
-
-    return result;
+	if (s1 != NULL)
+	{
+		s1_length = strlen(s1);
+	}
+	if (s2 != NULL)
+	{
+		s2_length = strlen(s2);
+	}
+	result = (char *)malloc(s1_length + s2_length + 1);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	p = result;
+	while (s1 != NULL && *s1 != '\0')
+	{
+		*p++ = *s1++;
+	}
+	while (s2 != NULL && *s2 != '\0')
+	{
+		*p++ = *s2++;
+	}
+	*p = '\0';
+	return (result);
 }
 
