57843fc59848e0b011cc8a314e7694735f4acf49
Enock Asant <asanteenoch10@gmail.com>
2023-04-17 14:57:38 -0700

    update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ * free_dog - frees dogs.
+ * @d : instance of dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
