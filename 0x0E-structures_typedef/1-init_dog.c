517bce1db753991f4eb02d30128111bceed16274
Enock Asant <asanteenoch10@gmail.com>
2023-04-17 13:14:15 -0700

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <string.h>
 /**
  * init_dog - initialize varaibles of struct dog
  * @d : insatnce of dog
@@ -9,7 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (strlen(name) > 0 && strlen(owner) > 0 && age > 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
