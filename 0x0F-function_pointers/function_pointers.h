6f0a36e72354bb903fb4210db03383c8ac39c81e
Enock Asant <asanteenoch10@gmail.com>
2023-04-19 10:24:22 -0700

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTIO_POINTERS
 
 #include <stdio.h>
+#include <string.h>
 void _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
