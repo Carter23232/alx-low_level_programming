29cf306b938d7f0b273ecefbb7e0d225761ed1a0
Enock Asant <asanteenoch10@gmail.com>
2023-04-20 14:47:16 -0700

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,6 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _strlen(const char *format, char *str);
+void print_a_char(char *separator, va_list args);
+void print_a_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_char_ptr(char *separator, va_list args);
 
 
 
