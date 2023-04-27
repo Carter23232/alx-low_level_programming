79f270d68c692884563941316665321129c30a24
Enock Asant <asanteenoch10@gmail.com>
2023-04-27 13:14:10 -0700

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+/**
+  * print_list - Prints all elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		nodeCount++;
+	}
+
+	return nodeCount;
+}
