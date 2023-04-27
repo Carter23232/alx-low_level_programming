f6a055c5672d715ddbc1d0ac3ead3cab0dbcf0d0
Enock Asant <asanteenoch10@gmail.com>
2023-04-27 13:40:05 -0700

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,12 @@
   */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        h = h->next;
-        count++;
-    }
-
-    return (count);
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
