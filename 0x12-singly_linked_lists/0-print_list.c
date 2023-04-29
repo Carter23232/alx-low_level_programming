125209c57d2049cc4afddb9fe87a8ceb7bd4824a
Enock Asant <asanteenoch10@gmail.com>
2023-04-29 13:08:41 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,16 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t nodeCount = 0;
+	size_t len = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
+			printf("[0] (nil)\n");
+		len++;
 		h = h->next;
-		nodeCount++;
 	}
-
-	return (nodeCount);
+	return (len);
 }
