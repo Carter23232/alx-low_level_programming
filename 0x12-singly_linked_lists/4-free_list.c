171281f883ad9284cd7e61ad0424429a5661ed92
Enock Asant <asanteenoch10@gmail.com>
2023-04-29 15:23:39 -0700

    update

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a linked list.
+ * @head: The pointer to the first node of linked list
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(current->str);
+		free(current);
+	}
+}
