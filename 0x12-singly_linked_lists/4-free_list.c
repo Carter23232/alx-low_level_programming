e47eb4c7ee06dcbf06723367a3af5c1698795e9d
Enock Asant <asanteenoch10@gmail.com>
2023-04-29 15:31:05 -0700

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,10 +9,11 @@
 void free_list(list_t *head)
 {
 	list_t *current;
-	current = head;
+
 	while (head != NULL)
 	{
-		current = current->next;
+		current = head;
+		head = head->next;
 		free(current->str);
 		free(current);
 	}
