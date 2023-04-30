0c7b070c6795551e5856b07013d1d8c7b4f558bb
Enock Asant <asanteenoch10@gmail.com>
2023-04-30 09:47:24 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		cur->n = n;
 		while (*head != NULL)
 		{
-			if (i == idx)
+			if (i == idx - 1)
 			{
 				nxt = (*head)->next;
 				(*head)->next = cur;
