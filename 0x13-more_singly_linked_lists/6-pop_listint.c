6eace07b582ce049b549a281b273a217f19d7937
Enock Asant <asanteenoch10@gmail.com>
2023-04-30 08:42:26 -0700

    update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,19 +3,21 @@
 /**
   * pop_listint -  deletes the head node
   * @head: head of list
-  *
   * Return: int data
   */
 
 int pop_listint(listint_t **head)
 {
+	listint_t *cur;
 	int n;
 
 	if (head == NULL)
 	{
 		return (0);
 	}
+	cur = *head;
 	n = (*head)->n;
-	free(*head);
+	*head = (*head)->next;
+	free(cur);
 	return (n);
 }
