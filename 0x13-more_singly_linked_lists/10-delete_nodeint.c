184a1bf4540519d2fe5175283baded0af067a0fb
Enock Asant <asanteenoch10@gmail.com>
2023-04-30 11:17:06 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,8 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 				cur = nxt->next;
 				nxt->next = cur->next;
 				free(cur);
-				cur = NULL;
-				return (1);
+			        return (1);
 			}
 			nxt = nxt->next;
 			i++;
