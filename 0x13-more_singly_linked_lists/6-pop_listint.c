31c8d0738dff781ec7e83d6608fbe46a5fc1e649
Enock Asant <asanteenoch10@gmail.com>
2023-04-30 08:48:04 -0700

    update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *cur;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return (0);
 	}
@@ -19,6 +19,5 @@ int pop_listint(listint_t **head)
 	n = (*head)->n;
 	*head = (*head)->next;
 	free(cur);
-	cur = NULL;
 	return (n);
 }
