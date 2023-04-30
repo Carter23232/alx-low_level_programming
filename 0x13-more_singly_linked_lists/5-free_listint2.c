2d7e72c33332cd6c152f9aa9b1ccf2d9e8b02f1a
Enock Asant <asanteenoch10@gmail.com>
2023-04-30 07:05:34 -0700

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,17 @@ void free_listint2(listint_t **head)
 {
 	listint_t *cur;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+	else
 	{
-		cur = (*head);
-		*head = (*head)->next;
-		free(cur);
+		while (*head != NULL)
+		{
+			cur = (*head);
+			*head = (*head)->next;
+			free(cur);
+		}
 	}
 	free(*head);
-	head = NULL;
+	*head = NULL;
 }
