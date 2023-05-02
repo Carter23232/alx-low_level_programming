c4fe99e42f71945b8a167fb935bc1e81f5539fad
Enock Asant <asanteenoch10@gmail.com>
2023-05-02 09:47:51 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,38 +7,25 @@
 */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t i = 0, j = 0, visited_chk = 0;
-    listint_t **visited_nodes = malloc(sizeof(listint_t *) * 1024);
-    listint_t *ptr;
+	size_t counts = 0;
+	listint_t *cur, *nxt;
 
-    while (*h != NULL)
-    {
-        if (!visited_chk)
-        {
-            visited_nodes[i++] = *h;
-            ptr = *h;
-            *h = (*h)->next;
-            free(ptr);
-        }
-        else
-        {
-            free(*h);
-            *h = NULL;
-            break;
-        }
+	cur = *h;
+        *h = NULL;
 
-        j = 0;
-        while (j < i)
-        {
-            if (visited_nodes[j] == *h)
-            {
-                visited_chk = 1;
-            }
-            j++;
-        }
-    }
-    free(*h);
-    *h = NULL;
-    free(visited_nodes);
-    return (i);
+	while (cur != NULL)
+	{
+		nxt= cur->next;
+		if(nxt <= cur)
+		{
+			free(cur);
+			counts++;
+		}
+		else
+		{
+			break;
+		}
+		cur = nxt;
+	}
+        return (counts);
 }
