10f7be06968d14f8b0e43bde4b00c5be55ba0cfc
Carter23232 <asanteenoch10@gmail.com>
2023-08-31 22:02:34 +0200

    update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * binary_to_uint - free a listint_t linked list
+ * @b: pointer to head
+ * Return: unsigned number
+*/
+int i_pow (int num, int counter);
+int todigit(char b);
+
+unsigned int binary_to_uint(const char *b)
+{
+	int counter = 0, value = 0;
+
+	if (b == NULL)
+		return (0);
+	while(*b != '\0')
+	{
+		if  (*b == '1' || *b == '0')
+			counter++;
+		else return (0);
+		b++;
+	}
+	while (counter >= 0)
+	{
+		counter--;
+		while (*b != '\0')
+		{
+			value += i_pow(2, counter) *todigit(*b);
+		}
+		b++;
+	}
+	return (value);
+}
+int i_pow (int num, int counter)
+{
+	int res = num;
+	while (counter < 0)
+	{
+		res *= num;
+		counter--;
+	}
+	return (res);
+}
+
+int todigit(char b)
+{
+	if (b == '1')
+		return (1);
+        return (0);
+}
