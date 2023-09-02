f1dfad3c1f1a480247274ec24891744af8e0b497
Carter23232 <asanteenoch10@gmail.com>
2023-09-03 01:20:24 +0200

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,16 +9,18 @@ void print_binary(unsigned long int n)
 	int bit_position = (sizeof(n) * 8) - 1;
 	unsigned long int bit = 0;
 
-	if (n == 0)
+	if (n <= 0)
+	{
 		_putchar('0');
+		return;
+	}
 	while (bit_position >= 0)
 	{
 		bit = n >> bit_position;
 		if (1 & bit)
 			_putchar('1');
-		else
+		else if (bit > 0)
 			_putchar('0');
 		bit_position--;
 	}
-	_putchar('\n');
 }
