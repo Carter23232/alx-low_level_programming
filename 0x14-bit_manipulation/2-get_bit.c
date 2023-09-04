0c29b8cf9275821575e3959714edf9f06f94f360
Carter23232 <asanteenoch10@gmail.com>
2023-09-04 17:14:04 +0200

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-int *binary(unsigned long int n);
+
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: number
@@ -10,37 +10,12 @@ int *binary(unsigned long int n);
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (binary(n)[31 - index] == 0 || binary(n)[31 - index] == 1)
-		return (binary(n)[31 - index]);
-	return (-1);
-}
+	unsigned long int r_shifted = n;
 
-
-/**
- * binary - converts numbers to an array of binary
- * @n: number
- * Return: returns array of binary
- */
-int *binary(unsigned long int n)
-{
-	int bit_position = (sizeof(n) * 8) - 1;
-	unsigned long int bit = 0;
-	int *arr_binary = malloc(sizeof(int) * 31);
-	int *result = arr_binary;
-
-	while (bit_position >= 0)
-	{
-		bit = n >> bit_position;
-		if (1 & bit)
-		{
-			*arr_binary = 1;
-		}
-		else if (bit > 0)
-		{
-			*arr_binary = 0;
-		}
-		arr_binary++;
-		bit_position--;
-	}
-	return (result);
+	if (index > (sizeof(n) * 8) - 1)
+		return (-1);
+	r_shifted = n >> index;
+	if (1 & r_shifted)
+		return (1);
+	return (0);
 }
