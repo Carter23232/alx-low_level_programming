621981d15da604f02f9274b9e444ebc3709e68f8
Carter23232 <asanteenoch10@gmail.com>
2023-09-06 00:31:57 +0200

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,34 +7,35 @@
 */
 int main(int argc, char *argv[])
 {
-	const char *E_msg_arg = "Usage: cp file_from file_to\n",
-		*E_src = "Error: Can't read from file ", *E_dest = "Error: Can't write to ";
 	char *buf[BUFSIZ];
-	int dest, src, _read = 1, written;
+	int dest, src, _read, written;
 	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s", E_msg_arg), exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	src = open(argv[1], O_RDONLY);
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
 	if (src == -1)
 	{
-		dprintf(STDERR_FILENO, "%s %s %s", E_src, argv[1], "\n"), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (dest == -1)
 	{
-		dprintf(STDERR_FILENO, "%s %s %s", E_dest, argv[2], "\n"), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
+	_read = 1;
 	while (_read)
 	{
-		_read = read(src, buf, 1024);
+		_read = read(src, buf, BUFSIZ);
 		if (_read == -1)
-			dprintf(STDERR_FILENO, "%s %s %s", E_src, argv[1], "\n"), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		written = write(dest, buf, BUFSIZ);
 		if (written == -1 || written != _read)
-			dprintf(STDERR_FILENO, "%s %s %s", E_dest, argv[2], "\n"), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(src) == -1)
 	{
