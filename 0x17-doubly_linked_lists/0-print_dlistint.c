9ca87aa8a8eaa668f2977490533045a88e6ec70f
Carter23232 <asanteenoch10@gmail.com>
2024-02-22 19:43:51 +0100

    update

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/*
+ * print_dlistint - doubly linked list
+ * @h: head of list
+ * Return: 0 if none or len
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+	if (!h)
+		return (0);
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
