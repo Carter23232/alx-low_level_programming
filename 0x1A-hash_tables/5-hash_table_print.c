bcc8148586a6621c2fc44f1d73f75415a2fd19df
Carter23232 <asanteenoch10@gmail.com>
2024-03-05 00:22:27 +0100

    update

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash_table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int printed = 0;
+	hash_node_t *temp;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	printf("{");
+	while (i <= ht->size)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (printed)
+				printf(", ");
+
+			printf("'%s' : '%s'", temp->key, temp->value);
+			printed = 1;
+			temp = temp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
+
