3b5d0ff4637125767d11a8793d303a4be68e93d4
Carter23232 <asanteenoch10@gmail.com>
2024-03-04 22:16:04 +0100

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *temp;
 
 	if (!key || !ht)
-		return 0;
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
@@ -25,14 +25,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(temp->value);
 			temp->value = strdup(value);
-			return 1;
+			return (1);
 		}
 		temp = temp->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
-		return 0;
+		return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
@@ -41,11 +41,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node->key);
 		free(new_node->value);
 		free(new_node);
-		return 0;
+		return (0);
 	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	return 1;
+	return (1);
 }
