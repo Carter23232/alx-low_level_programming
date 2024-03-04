6a6e8bb96daea97072c0c80a698c8057e368ad55
Carter23232 <asanteenoch10@gmail.com>
2024-03-04 18:26:54 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	n_hT  = (hash_table_t *)malloc(sizeof(hash_node_t));
 	if (n_hT)
+	{
 		n_hT->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+		n_hT->size = size;
+	}
 	while (size)
 	{
 		if (n_hT)
@@ -22,4 +25,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	return (n_hT);
 }
+int main(void)
+{
+	hash_table_t *ht;
+	size_t i;
 
+	ht = hash_table_create(2048);
+	if (!ht || ht->size != 2048 || !ht->array)
+		return (EXIT_FAILURE);
+	/*Loop through the array, should not SEGV*/
+	for (i = 0; i < 2048 && ht->array[i]; ++i);
+	return (EXIT_SUCCESS);
+}
