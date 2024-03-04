451975de6678c8822086bda2cb54bc74608164a9
Carter23232 <asanteenoch10@gmail.com>
2024-03-04 23:38:55 +0100

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,9 +9,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *new_n;
 
 	if (!ht || !key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	if (ht->array[index]->next != NULL)
+	{
+		new_n = ht->array[index];
+		while (new_n)
+		{
+			if (strcmp(new_n->key, key) == 0)
+				return (new_n->value);
+			new_n = new_n->next;
+		}
+	}
 	return (ht->array[index]->value);
 }
