9216d69cc796e442dd6b2de9a82b2bda46ea60fd
Carter23232 <asanteenoch10@gmail.com>
2024-03-04 18:44:06 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	n_hT  = (hash_table_t *)malloc(sizeof(hash_node_t));
 	if (n_hT)
 	{
-		n_hT->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+		n_hT->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size - 1);
 		n_hT->size = size;
 	}
 	return (n_hT);
